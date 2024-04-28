56b402357ab894babab28686db735c439be135c1
fiezaeza <fiezanm0404@gmail.com>
2024-04-28 08:44:32 +0700

    form

---
+++ lib/widgets/profile/buy_list_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class BuyListProfile extends StatefulWidget {
+  const BuyListProfile({super.key});
+
+  @override
+  State<BuyListProfile> createState() => _BuyListProfileState();
+}
+
+class _BuyListProfileState extends State<BuyListProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/widgets/profile/sell_list_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class SellListProfile extends StatefulWidget {
+  const SellListProfile({super.key});
+
+  @override
+  State<SellListProfile> createState() => _SellListProfileState();
+}
+
+class _SellListProfileState extends State<SellListProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
