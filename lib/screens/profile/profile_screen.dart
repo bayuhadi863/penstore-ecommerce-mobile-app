edf5a75d25cec33485e3959e7555a3245a9ba453
fiezaeza <fiezanm0404@gmail.com>
2024-05-13 13:54:53 +0700

    tabbar profile

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -6,6 +6,7 @@ import 'package:penstore/controller/auth/logout_controller.dart';
 import 'package:penstore/widgets/profile/add_product_widget.dart';
 import 'package:penstore/widgets/profile/buy_list_widget.dart';
 import 'package:penstore/widgets/profile/profile_image_widget.dart';
+import 'package:penstore/widgets/profile/profile_tab.dart';
 import 'package:penstore/widgets/profile/sell_list_widget.dart';
 
 class ProfileScreen extends StatefulWidget {
@@ -15,10 +16,18 @@ class ProfileScreen extends StatefulWidget {
   State<ProfileScreen> createState() => _ProfileScreenState();
 }
 
-class _ProfileScreenState extends State<ProfileScreen> {
+class _ProfileScreenState extends State<ProfileScreen>
+    with SingleTickerProviderStateMixin {
   final userController = Get.put(UserController());
   //final _formKey = GlobalKey<FormState>();
   final FocusNode _searchFocusNode = FocusNode();
+  late TabController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = new TabController(length: 3, vsync: this);
+  }
 
   @override
   void dispose() {
@@ -44,8 +53,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                 ),
               ),
               const Padding(
-                padding:
-                    EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
+                padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
@@ -74,25 +82,40 @@ class _ProfileScreenState extends State<ProfileScreen> {
             endIndent: 20,
             color: Colors.black,
           ),
-          Container(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                GestureDetector(
-                  onTap: () => BuyListProfile(),
-                  child: Text("Beli"),
+          const AddProductForm(),
+          new Container(
+            child: new TabBar(
+              controller: _controller,
+              tabs: [
+                Tab(
+                  text: 'Beli',
                 ),
-                GestureDetector(
-                  onTap: () => SellListProfile(),
-                  child: Text("Jual"),
+                Tab(
+                  text: 'Jual',
                 ),
-                GestureDetector(
-                  child: Text("Dibeli"),
-                )
+                Tab(
+                  text: 'Dibeli',
+                ),
               ],
             ),
           ),
-          const AddProductForm(),
+          new Container(
+            height: 250,
+            child: new TabBarView(
+              controller: _controller,
+              children: <Widget>[
+                Container(
+                  child: Text("buy list"),
+                ),
+                Container(
+                  child: Text("sell list"),
+                ),
+                Container(
+                  child: Text("bought list"),
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
---
+++ lib/widgets/profile/profile_tab.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class ProfileTab extends StatefulWidget {
+  const ProfileTab({super.key});
+
+  @override
+  State<ProfileTab> createState() => _ProfileTabState();
+}
+
+class _ProfileTabState extends State<ProfileTab> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      initialIndex: 1,
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('TabBar Sample'),
+          bottom: const TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(Icons.cloud_outlined),
+              ),
+              Tab(
+                icon: Icon(Icons.beach_access_sharp),
+              ),
+              Tab(
+                icon: Icon(Icons.brightness_5_sharp),
+              ),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: <Widget>[
+            Center(
+              child: Text("It's cloudy here"),
+            ),
+            Center(
+              child: Text("It's rainy here"),
+            ),
+            Center(
+              child: Text("It's sunny here"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
