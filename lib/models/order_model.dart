bfc63e36fd0d1607f1cb0bcc0ff4976962e9c6b4
Bayu Hadi Leksana <bayuhadi863@gmail.com>
2024-05-20 07:16:12 +0700

    comment unfinished code

--- lib/models/order_model.dart
+++ lib/models/order_model.dart
@@ -1,8 +1,8 @@
-class OrderModel {
-  final String? id;
-  final String userId;
-  final String cartId;
-  final int totalPrice;
-  final String status;
-  final DateTime? createdAt;
-}
+// class OrderModel {
+//   final String? id;
+//   final String userId;
+//   final String cartId;
+//   final int totalPrice;
+//   final String status;
+//   final DateTime? createdAt;
+// }
