3d3ff28ee10dbfb3760bf2a3e02738d658f47614
M. Ainur Ramadhan <134328737+Ramadhan1710@users.noreply.github.com>
2024-05-26 13:53:24 +0700

    printilan

--- lib/widgets/chat/chat_card.dart
+++ lib/widgets/chat/chat_card.dart
@@ -1,6 +1,8 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
+
 import 'package:penstore/models/roomChat_model.dart';
 
 // import '../../../constants.dart';
@@ -9,10 +11,9 @@ class ChatCard extends StatelessWidget {
   const ChatCard({
     super.key,
     required this.roomChat,
-    required this.recieverId,
-    required chat,
     required this.isSeen,
-  }) : super(key: key);
+    required this.recieverId, required chat,
+  });
 
   final RoomChatModel roomChat;
   final bool isSeen;
